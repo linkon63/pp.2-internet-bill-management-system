efeae22313fd2e67d26a4a92d4833d04627d20ea
linkon63 <m.alinkon10@gmail.com>
2024-01-26 16:15:33 +0600

    ---

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { PageRoutes } from "./components/route/Router";
-
+import { PrimeReactProvider } from 'primereact/api';
 function App() {
   const router = createBrowserRouter(PageRoutes);
-  return <RouterProvider router={router} />;
+  return <PrimeReactProvider>
+    <RouterProvider router={router} />
+  </PrimeReactProvider>
 }
 
 export default App;
--- src/components/client/home/ClientHome.jsx
+++ src/components/client/home/ClientHome.jsx
@@ -37,13 +37,16 @@ export default function ClientHome() {
   return (
     <div className="h-full bg-white">
       <div className="h-11/12 border-3">
+
         <div className="border w-100  100 p-3 rounded flex justify-between items-center">
           <h2 className="font-bold text-2xl px-2">Welcome to Net Vision</h2>
+
           <div className="border rounded-3xl p-3 bg-indigo-600">
             <BsFillPersonFill className="text-white" />
           </div>
         </div>
-        <div className="pt-4 grid grid-gap gap-4 w-full grid-cols-1 md:grid-cols-1 lg:grid-cols-3 ps-4 pe-4">
+
+        {/* <div className="pt-4 grid grid-gap gap-4 w-full grid-cols-1 md:grid-cols-1 lg:grid-cols-3 ps-4 pe-4">
           <div className="bg-sky-500 rounded-xl p-4">
             <div className="p-4 text-white font-bold flex justify-between items-center">
               <p className="flex justify-start items-center text-lg">
@@ -71,7 +74,87 @@ export default function ClientHome() {
               <p className="text-lg font-light">5 recent</p>
             </div>
           </div>
+        </div> */}
+        <div class="col-12 md:col-6 lg:col-3">
+          <div class="surface-card shadow-2 p-3 border-round">
+            <div class="flex justify-content-between mb-3">
+              <div>
+                <span class="block text-500 font-medium mb-3">Orders</span>
+                <div class="text-900 font-medium text-xl">152</div>
+              </div>
+              <div class="flex align-items-center justify-content-center bg-blue-100 border-round" style="width:2.5rem;height:2.5rem">
+                <i class="pi pi-shopping-cart text-blue-500 text-xl"></i>
+              </div>
+            </div>
+            <span class="text-green-500 font-medium">24 new </span>
+            <span class="text-500">since last visit</span>
+          </div>
         </div>
+        {/* <div class="surface-ground px-4 py-5 md:px-6 lg:px-8">
+          <div class="grid">
+            <div class="col-12 md:col-6 lg:col-3">
+              <div class="surface-card shadow-2 p-3 border-round">
+                <div class="flex justify-content-between mb-3">
+                  <div>
+                    <span class="block text-500 font-medium mb-3">Orders</span>
+                    <div class="text-900 font-medium text-xl">152</div>
+                  </div>
+                  <div class="flex align-items-center justify-content-center bg-blue-100 border-round" style="width:2.5rem;height:2.5rem">
+                    <i class="pi pi-shopping-cart text-blue-500 text-xl"></i>
+                  </div>
+                </div>
+                <span class="text-green-500 font-medium">24 new </span>
+                <span class="text-500">since last visit</span>
+              </div>
+            </div>
+            <div class="col-12 md:col-6 lg:col-3">
+              <div class="surface-card shadow-2 p-3 border-round">
+                <div class="flex justify-content-between mb-3">
+                  <div>
+                    <span class="block text-500 font-medium mb-3">Revenue</span>
+                    <div class="text-900 font-medium text-xl">$2.100</div>
+                  </div>
+                  <div class="flex align-items-center justify-content-center bg-orange-100 border-round" style="width:2.5rem;height:2.5rem">
+                    <i class="pi pi-map-marker text-orange-500 text-xl"></i>
+                  </div>
+                </div>
+                <span class="text-green-500 font-medium">%52+ </span>
+                <span class="text-500">since last week</span>
+              </div>
+            </div>
+            <div class="col-12 md:col-6 lg:col-3">
+              <div class="surface-card shadow-2 p-3 border-round">
+                <div class="flex justify-content-between mb-3">
+                  <div>
+                    <span class="block text-500 font-medium mb-3">Customers</span>
+                    <div class="text-900 font-medium text-xl">28441</div>
+                  </div>
+                  <div class="flex align-items-center justify-content-center bg-cyan-100 border-round" style="width:2.5rem;height:2.5rem">
+                    <i class="pi pi-inbox text-cyan-500 text-xl"></i>
+                  </div>
+                </div>
+                <span class="text-green-500 font-medium">520  </span>
+                <span class="text-500">newly registered</span>
+              </div>
+            </div>
+            <div class="col-12 md:col-6 lg:col-3">
+              <div class="surface-card shadow-2 p-3 border-round">
+                <div class="flex justify-content-between mb-3">
+                  <div>
+                    <span class="block text-500 font-medium mb-3">Comments</span>
+                    <div class="text-900 font-medium text-xl">152 Unread</div>
+                  </div>
+                  <div class="flex align-items-center justify-content-center bg-purple-100 border-round" style="width:2.5rem;height:2.5rem">
+                    <i class="pi pi-comment text-purple-500 text-xl"></i>
+                  </div>
+                </div>
+                <span class="text-green-500 font-medium">85 </span>
+                <span class="text-500">responded</span>
+              </div>
+            </div>
+          </div>
+        </div> */}
+
         <div className="">
           <div className="w-6/6">
             <ClientDashboardTable
